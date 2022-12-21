9b956aaf60663173d0fa0dbdeba34f727d1bc78e
Sanja Vukelic <svukelic@vm1.avisto-devops.net>
2022-12-21 10:50:40 +0100

    pylint and black

--- configjen.py
+++ configjen.py
@@ -2,29 +2,25 @@ from configparser import ConfigParser
 
 config = ConfigParser()
 
-config['GITLAB'] = {
-    "hostGitlab": 'http://192.168.10.200:8083/',
-    "passwordGitlab": "glpat-xAyPHPx7pJfC2knxNnHx"
+config["GITLAB"] = {
+    "hostGitlab": "http://192.168.10.200:8083/",
+    "passwordGitlab": "glpat-xAyPHPx7pJfC2knxNnHx",
 }
 
-config['JENKINS'] = {
+config["JENKINS"] = {
     "hostJenkins": "http://192.168.10.200:8082/",
     "usernameJenkins": "svukelic",
     "passwordJenkins": "114cf2d71d18de7dd85b1ca7a2ecee0067",
-    "job": "Test jenkinsss"
-
+    "job": "Test jenkinsss",
 }
 
-config['JIRA'] = {
-    "hostJira": 'http://192.168.10.200:8086/',
-    "API_token_jira": 'OTUwNjA0MDUyNjgxOl7cmORGLvsRpUyI2S7Cj4e5DcJE',
-    "issue_key": "JEN-1"
+config["JIRA"] = {
+    "hostJira": "http://192.168.10.200:8086/",
+    "API_token_jira": "OTUwNjA0MDUyNjgxOl7cmORGLvsRpUyI2S7Cj4e5DcJE",
+    "issue_key": "JEN-1",
 }
 
-config['HOST'] = {
-    "host": '192.168.10.200',
-    "port": '3539'
-}
+config["HOST"] = {"host": "192.168.10.200", "port": "3539"}
 
 with open("paramjenkinsadmin.ini", "w") as f:
     config.write(f)
--- scriptscript.py
+++ scriptscript.py
@@ -1,13 +1,9 @@
-from flask import Flask, request, abort
+import json
+import time
+from configparser import ConfigParser
 from flask import Flask, request
 from api4jenkins import Jenkins
-from configparser import ConfigParser
 from jira import JIRA
-import json
-import gitlab
-import jenkins
-import requests
-import time
 
 
 
@@ -17,50 +13,48 @@ config.read("paramjenkinsadmin.ini")
 app = Flask(__name__)
 
 @app.route('/gitlab',methods=['POST'])
-def foo():
-   data = json.loads(request.data)
-   print("GITLAB Project")
-   print (f"Project: {data['project']}\n"
+def gitlab_push():
+    data = json.loads (request.data)
+    print ("GITLAB Project")
+    print (f"Project: {data['project']}\n"
           f"Event: {data['object_kind']}\n\n")
+
     #push
-   if data['object_kind'] == 'push':
-      config_data = config["JENKINS"]
-      hostJenkins = config_data['hostjenkins']
-      usernameJenkins = config_data['usernamejenkins']
-      passwordJenkins = config_data['passwordjenkins']
-      server = Jenkins(hostJenkins, auth=(usernameJenkins,passwordJenkins))
-      
-      job_name = config_data['job']
-      job = server.get_job(job_name) 
-      item = job.build()
-      while not item.get_build():
-         time.sleep(1)
-      build = item.get_build()
-      while build.building:
-         time.sleep(1)
-      while build.result is None:
-         time.sleep(1)
-      print (build.result)
-      resultt = build.result
+    if data ['object_kind'] == 'push':
+        config_data = config ["JENKINS"]
+        host_jenkins = config_data ['hostjenkins']
+        username_jenkins = config_data ['usernamejenkins']
+        password_jenkins = config_data ['passwordjenkins']
+        server = Jenkins (host_jenkins, auth = (username_jenkins, password_jenkins))
+        job_name = config_data ['job']
+        job = server.get_job (job_name) 
+        item = job.build ()
+        while not item.get_build ():
+            time.sleep (1)
+        build = item.get_build ()
+        while build.building:
+            time.sleep (1)
+        while build.result is None:
+            time.sleep (1)
+        print (build.result)
+        resultt = build.result
 
-      config_data3 = config["JIRA"]
-      hostJira = config_data3['hostjira']
-      API_token_jira = config_data3['api_token_jira']
-      jira = JIRA(hostJira , token_auth=(API_token_jira))
+        config_data3 = config ["JIRA"]
+        host_jira = config_data3 ['hostjira']
+        api_token_jira = config_data3 ['api_token_jira']
+        jira = JIRA (host_jira , token_auth = (api_token_jira))
 
-      issueja = config_data3['issue_key']
-      issue = jira.issue(issueja)
-      jira.add_comment(issue, f"Pipeline result: {resultt}.")
-
-   
-   return "OK"
+        issueja = config_data3 ['issue_key']
+        issue = jira.issue (issueja)
+        jira.add_comment (issue, f"Pipeline result: {resultt}.")
+    return "OK"
 
 
 
 
 
 if __name__ == '__main__':
-    config_data4 = config["HOST"]
-    host = config_data4['host']
-    port = config_data4['port']
-    app.run(host= host, port= port)
+    config_data4 = config ["HOST"]
+    host = config_data4 ['host']
+    port = config_data4 ['port']
+    app.run (host= host, port= port)
